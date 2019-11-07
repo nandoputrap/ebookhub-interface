42d9bd48dab9a911d95f48014b86eb186213d91a
Nando P. Pratama <nandoputrap@gmail.com>
2019-11-07 11:11:27 +0700

    ebookhub front-end sampai product slider - by ndo_kun

---
+++ index.php
@@ -0,0 +1,184 @@
+<?php
+  require_once("templates/header.php");
+?>
+
+
+<!-- Begin Welcome -->
+<div class="welcome">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 greetings">
+        <h1 id="greetings-1">Hi, kami Ebookhub!</h1>
+        <h3 id="greetings-2">Platform untuk menerbitkan e-book, membaca e-book, dan membeli e-book yang tepat untukmu!</h3>
+        <h3 id="greetings-3">Ebookhub ingin membantu minat baca di Indonesia, ayo mulai membaca dan menerbitkan sekarang!</h3>
+
+        <div class="col-md-6">
+          <button type="button" class="btn btn-primary btn-block btn-ebookhub">Mulai Baca</button>
+        </div>
+        <div class="col-md-6">
+          <button type="button" class="btn btn-primary btn-block btn-ebookhub">Mulai Terbitkan</button>
+        </div>
+      </div>
+
+      <div class="col-md-6 devices text-center">
+        <img src="images/devices.png" alt="" class="img-responsive">
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Welcome -->
+
+<!-- Begin How-to-use -->
+<div class="how-to-use">
+  <div class="container">
+    <div class="row">
+
+        <div class="col-md-12">
+          <h1 class="how-to-use-title text-center">Bagaimana menerbitkan buku di Eboookhub?</h1>
+        </div>
+
+        <div class="col-md-4 col-sm-12">
+          <div class="box-how-to-use box-shadow text-center">
+            <i class="fa fa-upload fa-how-to-use" aria-hidden="true"></i>
+            <h2>Unggah</h2>
+            <p>Unggah draft e-book mu dan kami akan segera melakukan pengecekan dan kelayakan terbit.</p>
+
+          </div>
+        </div>
+
+        <div class="col-md-4 col-sm-12">
+          <div class="box-how-to-use box-shadow text-center">
+            <i class="fa fa-edit fa-how-to-use" aria-hidden="true"></i>
+            <h2>Proses Edit</h2>
+            <p>Proses pengeditan ± 1 bulan, apabila draft e-book mu layak, maka akan dihubungi oleh tim kami dan melanjutkan ke proses negosiasi harga jual buku.</p>
+
+          </div>
+        </div>
+
+        <div class="col-md-4 col-sm-12">
+          <div class="box-how-to-use box-shadow text-center">
+            <i class="fa fa-book fa-how-to-use" aria-hidden="true"></i>
+            <h2>Terbit</h2>
+            <p>Unggah draft e-book mu dan kami akan segera melakukan pengecekan dan kelayakan terbit.</p>
+
+          </div>
+        </div>
+
+
+    </div>
+</div>
+</div>
+<!-- End How-to-use -->
+
+<div class="baru-diterbitkan">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="text-center">Baru Diterbitkan</h1>
+      </div>
+
+      <div class="col-lg-12">
+        <div id="owl-demo" class="owl-carousel owl-theme">
+
+          <div class="item">
+            <div class="card box-shadow">
+              <img class="card-img-top img-fluid" src="images/ebook-1.png" alt="card-img">
+              <div class="card-body">
+                <a href="index.php"><h3 class="card-title ebook-title"><strong>Judul buku</strong></h3></a>
+                <p class="card-text ebook-author">Nama Penulis</p>
+                <h4 class="card-title ebook-price"><strong>Rp. 100.000</strong></h4>
+                <a class="btn btn-lg btn-danger btn-beli text-capitalize"><i class="fa fa-shopping-cart"> &nbsp; Beli</i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="card box-shadow">
+              <img class="card-img-top img-fluid" src="images/ebook-1.png" alt="card-img">
+              <div class="card-body">
+                <a href="index.php"><h3 class="card-title ebook-title"><strong>Judul buku</strong></h3></a>
+                <p class="card-text ebook-author">Nama Penulis</p>
+                <h4 class="card-title ebook-price"><strong>Rp. 100.000</strong></h4>
+                <a class="btn btn-lg btn-danger btn-beli text-capitalize"><i class="fa fa-shopping-cart"> &nbsp; Beli</i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="card box-shadow">
+              <img class="card-img-top img-fluid" src="images/ebook-1.png" alt="card-img">
+              <div class="card-body">
+                <a href="index.php"><h3 class="card-title ebook-title"><strong>Judul buku</strong></h3></a>
+                <p class="card-text ebook-author">Nama Penulis</p>
+                <h4 class="card-title ebook-price"><strong>Rp. 100.000</strong></h4>
+                <a class="btn btn-lg btn-danger btn-beli text-capitalize"><i class="fa fa-shopping-cart"> &nbsp; Beli</i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="card box-shadow">
+              <img class="card-img-top img-fluid" src="images/ebook-1.png" alt="card-img">
+              <div class="card-body">
+                <a href="index.php"><h3 class="card-title ebook-title"><strong>Judul buku</strong></h3></a>
+                <p class="card-text ebook-author">Nama Penulis</p>
+                <h4 class="card-title ebook-price"><strong>Rp. 100.000</strong></h4>
+                <a class="btn btn-lg btn-danger btn-beli text-capitalize"><i class="fa fa-shopping-cart"> &nbsp; Beli</i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="card box-shadow">
+              <img class="card-img-top img-fluid" src="images/ebook-1.png" alt="card-img">
+              <div class="card-body">
+                <a href="index.php"><h3 class="card-title ebook-title"><strong>Judul buku</strong></h3></a>
+                <p class="card-text ebook-author">Nama Penulis</p>
+                <h4 class="card-title ebook-price"><strong>Rp. 100.000</strong></h4>
+                <a class="btn btn-lg btn-danger btn-beli text-capitalize"><i class="fa fa-shopping-cart"> &nbsp; Beli</i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="item">
+            <div class="card box-shadow">
+              <img class="card-img-top img-fluid" src="images/ebook-1.png" alt="card-img">
+              <div class="card-body">
+                <a href="index.php"><h3 class="card-title ebook-title"><strong>Judul buku</strong></h3></a>
+                <p class="card-text ebook-author">Nama Penulis</p>
+                <h4 class="card-title ebook-price"><strong>Rp. 100.000</strong></h4>
+                <a class="btn btn-lg btn-danger btn-beli text-capitalize"><i class="fa fa-shopping-cart"> &nbsp; Beli</i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="customNavigation">
+          <a class="btn prev">Previous</a>
+          <a class="btn next">Next</a>
+          <!-- <a class="btn play">Autoplay</a>
+          <a class="btn stop">Stop</a> -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+<!-- Begin partner -->
+
+<!-- End partner -->
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+<?php
+  require_once("templates/footer.php");
+?>
---
+++ js/custom.js
@@ -0,0 +1,51 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:3,
+//             nav:false
+//         },
+//         1000:{
+//             items:5,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// })
+
+
+
+
+    $(document).ready(function() {
+
+      var owl = $("#owl-demo");
+
+      owl.owlCarousel({
+          items : 3, //10 items above 1000px browser width
+          itemsDesktop : [1000,5], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,3], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 0
+          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+      });
+
+      // Custom Navigation Events
+      $(".next").click(function(){
+        owl.trigger('owl.next');
+      })
+      $(".prev").click(function(){
+        owl.trigger('owl.prev');
+      })
+      $(".play").click(function(){
+        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+      })
+      $(".stop").click(function(){
+        owl.trigger('owl.stop');
+      })
+
+    });
