97181dcde49a7f623ae50cac786e1998c1017f02
Nando P. Pratama <nandoputrap@gmail.com>
2019-11-19 19:52:10 +0700

    profil add

---
+++ profil.php
@@ -0,0 +1,49 @@
+<?php
+  require_once("templates/header.php");
+?>
+
+<div class="shop section-margin">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="item">
+          <div class="card  text-center card-product-details">
+            <img class='card-img-top img-circle img-fluid' src='images/avatar.png' alt='card-img'>
+            <!-- <h2>Nando Putra Pratama</h2> -->
+          </div>
+        </div>
+
+        <div class="panel panel-default sidebar-menu">
+          <div class="panel-harga">
+            <div class="panel-heading text-center">
+              <h3 class="panel-title">Nando Putra Pratama</h3>
+            </div>
+
+            <div class="panel-body">
+              <ul class="nav nav-pills nav-stacked category-menu">
+                <li>
+                  <a href="#">Edit Profil</a>
+                </li>
+                <li>
+                  <a href="#">Status Pengajuan</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-9 description-box">
+
+
+      </div>
+    </div>
+
+
+    </div>
+
+</div>
+
+<?php
+  require_once("templates/footer.php");
+?>
